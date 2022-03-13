fc3c78247090cf311bcd5ad8032ebbd6e66d8ca0
Qi <add_sp@outlook.com>
2022-03-14 00:07:14 +0800

    test

--- kong/timer.lua
+++ kong/timer.lua
@@ -983,7 +983,7 @@ function _M:once(name, callback, delay, ...)
     assert(type(delay) == "number", "expected `delay to be a number")
     assert(delay >= 0, "expected `delay` to be greater than or equal to 0")
 
-    if delay >= MAX_EXPIRE or (true and delay < 0.1)  then
+    if delay >= MAX_EXPIRE or (delay ~= 0 and delay < 0.1) or not self.configured then
         local ok, err = timer_at(delay, callback, ...)
         return ok ~= nil, err
     end
@@ -1000,7 +1000,7 @@ function _M:every(name, callback, interval, ...)
     assert(type(interval) == "number", "expected `interval to be a number")
     assert(interval > 0, "expected `interval` to be greater than or equal to 0")
 
-    if interval >= MAX_EXPIRE or interval < 0.1 then
+    if interval >= MAX_EXPIRE or interval < 0.1 or not self.configured then
         local ok, err = timer_every(interval, callback, ...)
         return ok ~= nil, err
     end
