8545336fe95d9b7b85cccc4483dc9efd38b18b83
Hans Huebner <hans.huebner@konghq.com>
2022-06-24 16:20:45 +0200

    Beginnings of a new upgrade test script

---
+++ scripts/test-upgrade-path.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -ex
+
+FROM_VERSION=2.8.1
+TO_VERSION=master
+NETWORK_NAME=migration-$FROM_VERSION-$TO_VERSION
+
+gojira up -t $FROM_VERSION --network $NETWORK_NAME > upgrade-up-$FROM_VERSION.log 2>&1
+gojira run -t $FROM_VERSION make dev > upgrade-make-dev-$FROM_VERSION.log 2>&1
+gojira up -t $TO_VERSION --alone --network $NETWORK_NAME > upgrade-up-$TO_VERSION.log 2>&1
+gojira run -t $TO_VERSION make dev > upgrade-make-dev-$TO_VERSION.log 2>&1
+gojira run -t $FROM_VERSION kong migrations bootstrap
+gojira run -t $TO_VERSION kong migrations up
+# test old version before
+# test new version migrating
+gojira run -t $TO_VERSION kong migrations finish
+# test new version after
