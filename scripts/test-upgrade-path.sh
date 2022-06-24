eb3ddb2d02129b4cafcd891ba5f1fac3e2593073
Hans Huebner <hans.huebner@wagestream.com>
2022-06-24 17:17:29 +0200

    trap errors and provide message

--- scripts/test-upgrade-path.sh
+++ scripts/test-upgrade-path.sh
@@ -6,6 +6,8 @@ FROM_VERSION=2.8.1
 TO_VERSION=master
 NETWORK_NAME=migration-$FROM_VERSION-$TO_VERSION
 
+trap "echo exiting because of error" 0
+
 gojira up -t $FROM_VERSION --network $NETWORK_NAME > upgrade-up-$FROM_VERSION.log 2>&1
 gojira run -t $FROM_VERSION make dev > upgrade-make-dev-$FROM_VERSION.log 2>&1
 gojira up -t $TO_VERSION --alone --network $NETWORK_NAME > upgrade-up-$TO_VERSION.log 2>&1
@@ -16,3 +18,5 @@ gojira run -t $TO_VERSION kong migrations up
 # test new version migrating
 gojira run -t $TO_VERSION kong migrations finish
 # test new version after
+
+trap "" 0
